d55a4122883d8790f13069fdfbeeb6112be59d09
Zachary Mayle <zam1998@gmail.com>
2019-11-12 20:38:07 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,9 +2,8 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein= "meat")
-veg1 = "carrot"
-veg2 = "Zucc"
-puts "Wow this #{veg1}, #{veg2} and #{protein} are so good!"
+  puts "What a nutritious meal!"
+  puts 
 
 
 
